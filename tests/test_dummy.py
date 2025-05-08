b37f7b61d0ad5df142f6148f5f64d2258c5a2e9f
Snegdha <snegdha.adusumilli@hpe.com>
2025-05-08 11:22:26 -0600

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -2,5 +2,3 @@ import os
 
 def test_env_check():
     print("GITHUB_ACTIONS =", os.getenv("GITHUB_ACTIONS"))
-    print("MORPHEUS_HOST =", os.getenv("MORPHEUS_HOST"))
-    assert os.getenv("MORPHEUS_HOST") is not None
